d6f5ecf871e39e68fdeeeaa359a4a72035a0b99d
Zephyr <171523581+gentlezephyr@users.noreply.github.com>
2024-06-13 19:29:58 -0300

    Melhorando a lógica - Criando uma forma de gerar resultados diferentes

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+function ready() {
+    console.log('DOM Carregado!');
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
 function aperta() {
+
     const UnHideIt = document.querySelector('#toggle-hidden');
     const UnHideItImage = document.querySelector('#image-hidden');
+    let randomNumber = Math.floor(Math.random() * 2);
 
-    UnHideIt.classList.toggle('hidden');
-    UnHideItImage.classList.toggle('hidden');
+    if (randomNumber === 0) {
+        UnHideIt.classList.remove('hidden');
+        UnHideItImage.classList.remove('hidden');
+        UnHideIt.innerHTML = 'ESTAMOS NAMORANDO AGORA!';
+        document.getElementById('background').style.backgroundColor = 'pink';
+    } else {
+        UnHideIt.classList.remove('hidden');
+        UnHideIt.innerHTML = 'VOCÊ ESTÁ SOLTEIRO(A), TENTE NOVAMENTE!';
+        UnHideItImage.classList.add('hidden');
+        document.getElementById('background').style.backgroundColor = 'black';
+    };
+
+    console.log(UnHideIt);
+    console.log(randomNumber);
+    
 }
