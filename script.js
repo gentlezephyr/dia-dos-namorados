ef8d05aed7ce897056cec7350e7c7ab807c67d84
Zephyr <171523581+gentlezephyr@users.noreply.github.com>
2024-06-14 23:28:51 -0300

    Melhorando o design

--- script.js
+++ script.js
@@ -9,17 +9,18 @@ function aperta() {
     const UnHideIt = document.querySelector('#toggle-hidden');
     const UnHideItImage = document.querySelector('#image-hidden');
     let randomNumber = Math.floor(Math.random() * 2);
-
+    
     if (randomNumber === 0) {
+        document.getElementById('irasumir').style.display = 'none';
         UnHideIt.classList.remove('hidden');
         UnHideItImage.classList.remove('hidden');
         UnHideIt.innerHTML = 'ESTAMOS NAMORANDO AGORA!';
-        document.getElementById('background').style.backgroundColor = 'pink';
+        document.getElementById('background').style.backgroundColor = '#c1121f';
     } else {
         UnHideIt.classList.remove('hidden');
         UnHideIt.innerHTML = 'VOCÊ ESTÁ SOLTEIRO(A), TENTE NOVAMENTE!';
         UnHideItImage.classList.add('hidden');
-        document.getElementById('background').style.backgroundColor = 'black';
+        document.getElementById('background').style.backgroundColor = '#231942';
     };
 
     console.log(UnHideIt);
